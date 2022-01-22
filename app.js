7bee90be72dc31594f685c662321b6c8f8cdb245
Himanshu664 <gs2012023@sgsitsindore.in>
2022-01-22 17:53:42 +0530

    Added https request to mailchimp

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-
+const https = require("https");
+const { url } = require("inspector");
+const { options } = require("request");
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,8 +12,42 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  var firstName = req.body.fName;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us20.api.mailchimp.com/3.0/lists/d4e4c5af0b";
+
+  const options = {
+    method: "POST",
+    auth: "him:3bc9eba7cda4412193449fed425a05bb-us20",
+  };
+
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
 });
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+
